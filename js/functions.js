c4902c6c9681841cac7430f34a7eabe785d957b4
Thiago Gaitkoski <thiagoweb.dvelo@gmail.com>
2020-10-20 15:21:23 -0300

    Adição do slide de depoimentos

--- js/functions.js
+++ js/functions.js
@@ -138,4 +138,71 @@ $(function(){
 
     }
 
+    /*
+        GOTO CONTATO
+    */
+
+    var directory = 'C:/GitHub/RMVeiculos/'
+
+    $('[goto=contato]').click(function() {
+        location.href = directory+'index.html?contato';
+        return false;
+    })
+
+    checkUrl();
+
+    function checkUrl() {
+        var url = location.href.split('/');
+        var curPage = url[url.length-1].split('?');
+
+        if(curPage[1] != undefined && curPage[1] == 'contato'){
+            $('header nav a').css('color','black');
+            $('footer nav a').css('color','black');
+            $('[goto=contato]').css('color','#EB2D2D');
+            $('html,body').animate({'scrollTop':$('#contato').offset().top});
+        }
+    
+    }
+        /*
+            MENU RESPONSIVO
+        */
+
+        $('.mb').click(function(){
+            $(this).find('ul').slideToggle();
+        })
+
+        /*
+            SISTEMA DE DEPOIMENTOS
+        */
+
+        var amtDepoimento = $('.depoimento-single p').length;
+        var curIndex = 0;
+
+        iniciarDepoimentos();
+        navegarDepoimentos();
+
+        function iniciarDepoimentos(){
+            $('.depoimento-single p').hide();
+            $('.depoimento-single p').eq(0).show();
+        }
+
+
+        function navegarDepoimentos() {
+            $('[next]').click(function(){
+                    curIndex++;
+                    if(curIndex >= amtDepoimento)
+                        curIndex = 0;
+                    $('.depoimento-single p').hide();
+                    $('.depoimento-single p').eq(curIndex).show();
+            })
+
+            $('[prev]').click(function() {
+                curIndex--;
+                if(curIndex < 0)
+                    curIndex = amtDepoimento - 1;
+                $('.depoimento-single p').hide();
+                $('.depoimento-single p').eq(curIndex).show();
+            })
+        }
+
 })
